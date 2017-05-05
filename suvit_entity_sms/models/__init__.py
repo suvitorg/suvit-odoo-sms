64d027f419a93b3962d26c61a3949c2f7aa3191f
Victor Safronovich <vsafronovich@gmail.com>
2017-05-05 11:28:53 +0500

    dont translate template_body

--- suvit_entity_sms/models/__init__.py
+++ suvit_entity_sms/models/__init__.py
@@ -1,4 +1,6 @@
 ﻿from . import history
+from . import template
+
 from . import handler
 from . import partner
 from . import tracker
---
+++ suvit_entity_sms/models/template.py
@@ -0,0 +1,8 @@
+﻿# -*- coding: utf-8 -*-
+from openerp import api, fields, models
+
+
+class EsmsTemplate(models.Model):
+    _inherit = "esms.templates"
+
+    template_body = fields.Text(translate=False)
