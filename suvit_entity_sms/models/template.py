6bd7b89c300b74bf4ba9ef279ac76127a5250b4e
Victor Safronovich <vsafronovich@gmail.com>
2017-05-06 15:13:15 +0500

    added some usefull filters for russian date to use in sms templates

--- suvit_entity_sms/models/template.py
+++ suvit_entity_sms/models/template.py
@@ -1,8 +1,30 @@
 ﻿# -*- coding: utf-8 -*-
+import pytz
+
 from openerp import api, fields, models
+from openerp.http import request
+
+from openerp.addons.entity_sms.esms_templates import mako_template_env
+
+def user_datetime(date_str):
+    if request:
+        env = request.env
+        user_tz = pytz.timezone(env.context.get('tz') or env.user.tz or 'UTC')
+    else:
+        user_tz = pytz.utc
+
+    return user_tz.localize(fields.Datetime.from_string(date_str)).astimezone(pytz.utc)
+
+
+def ru_date_format(dt):
+    return dt.strftime('%d.%m.%Y')
+
+mako_template_env.filters.update(user_datetime=user_datetime,
+                                 ru_date_format=ru_date_format)
 
 
 class EsmsTemplate(models.Model):
     _inherit = "esms.templates"
 
+    # dont use translation on template
     template_body = fields.Text(translate=False)
